e4b77d17c08720471fa83327ed92e671aea5fa16
DannyXjsu <daniel.nazar.br@gmail.com>
2021-02-24 19:12:49 -0300

    even more linux support fix

--- yeah dude/system_console.h
+++ yeah dude/system_console.h
@@ -5,10 +5,13 @@ void Clear() {
 	#ifdef _WIN32
 	system("cls");
 	#else
-	system("read - p \"Press any key to resume ...\"");
+	system("clear");
 	#endif
-
 }
 void Pause() {
+	#ifdef _WIN32
 	system("pause");
+	#else
+	system("read - p 'Press any key to resume ...'");
+	#endif
 }
